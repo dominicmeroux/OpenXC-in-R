927bf23cbc0e39dec9f80f2f69c7a4f36ba0bcd9
dominicmeroux <dmeroux@gmail.com>
2016-03-25 19:04:53 -0700

    R-MySQL_OpenXC_data_V1.0
    This is an R and MySQL approach to reading in data generated from
    OpenXC trace files.

---
+++ OpenXCanalysis2_Bluetooth_for_Github.R
@@ -0,0 +1,203 @@
+##################
+# OpenXC R MySQL #
+################################################################################################
+# Purpose is to read OpenXC JSON trace files into R. Make sure JSON is properly formatted      #
+# (if OpenXC VI was unplugged prior to end of data recording, remove partial final message).   #
+#                                                                                              #
+# After reading into R, OpenXC messages are transferred to a MySQL database for storage        #
+# which can be important if many trips are recorded. Data can then be queried from R or        #
+# another tool, or imported into visualization software (e.g. Tableau).                        #
+################################################################################################
+
+# Set working directory to access appropriate files.
+WD = # Insert working directory here
+setwd(WD)
+
+#########################################
+######## STEP 1 - Extracting JSON Data
+#########################################
+#install.packages("jsonlite")
+#install.packages("sqldf")
+#install.packages("plyr")
+#install.packages("RMySQL")
+library("jsonlite")
+library("sqldf")
+library("plyr")
+library("RMySQL")
+
+# IF TRACES HAVE BEEN ALREADY UPLOADED INTO MySQL DATABASE, ENSURE SUBSEQUENT RUNS OF THIS R SCRIPT DON'T RE-Do THE WORK
+# ELSE IF DOING THIS FOR THE FIRST TIME, IGNORE EXISTINGTRACES-RELATED COMMANDS
+# http://www.stat.berkeley.edu/~nolan/stat133/Fall05/lectures/SQL-R.pdf
+# Goal here is to obtain names of existing traces in MySQL db so these aren't re-uploaded. 
+DB_Name = # Insert Database Name Here
+DB_User = # Insert Database Username Here
+DB_Pass = # Insert Database Password Here
+drv = dbDriver("MySQL")
+con = dbConnect(drv, user=DB_User, dbname=DB_Name,
+                host="localhost", # Change "localhost" if host is different
+                password=DB_Pass)
+
+ExistingTraces = dbGetQuery(con, "SELECT DISTINCT trace FROM coordinates;")
+
+## Extract json OpenXC drive trace filenames
+files = list.files(path=".", pattern=".json")
+
+## Transform ExistingTraces data frame into a character vector
+ExistingTraces2 = character()
+for(i in 1:nrow(ExistingTraces)){
+  ExistingTraces2[i] = ExistingTraces[i,1]
+}
+## Remove filenames for existing traces. Expect the following error statement:
+# Error in if (gsub(".json", "", files[i]) == ExistingTraces2[j]) { : 
+#   missing value where TRUE/FALSE needed
+for(i in 1:length(files)){
+  for(j in 1:length(ExistingTraces2)){
+    if(gsub(".json", "", files[i])==ExistingTraces2[j]){
+      files = files[-i]
+    }
+  }
+}
+
+## Remaining trace names
+fnames = sapply(files, function(x){gsub(".json", "", x)})
+
+# Read in streamed data: http://stackoverflow.com/questions/26519455/error-parsing-json-file-with-the-jsonlite-package
+# Expect warning messages: incomplete final line found on 'filename'
+data = list()
+names = character()
+dfs = list()
+for(i in 1:length(files)){
+  data[[i]] = data.frame(stream_in(file(files[i])))
+  data[[i]] = data[[i]][-3]
+}
+# Add filenames into a column 
+# Expect warning messages: number of items to replace is not a multiple of replacement length
+for(i in 1:length(files)){
+  names[i] = rep(fnames[i], times = nrow(data[[i]]))
+  dfs[[i]] = data.frame(names[i], data[[i]])
+}
+# Merge dataframe list into one large dataframe
+OpenXC_data = ldply(dfs, data.frame)
+# Rename dataset identifier column
+colnames(OpenXC_data)[1] = "trace"
+
+# List variable names to get a summary
+#Variables = sqldf("select count(*), trace, name from OpenXC_data group by trace, name;")
+
+OpenXC_data$timestamp = as.integer(OpenXC_data$timestamp)
+
+accelerator_pedal_position = OpenXC_data[which(OpenXC_data$name == "accelerator_pedal_position"),]
+brake_pedal_status = OpenXC_data[which(OpenXC_data$name == "brake_pedal_status"),]
+engine_speed = OpenXC_data[which(OpenXC_data$name == "engine_speed"),]
+fuel_consumed_since_restart = OpenXC_data[which(OpenXC_data$name == "fuel_consumed_since_restart"),]
+fuel_level = OpenXC_data[which(OpenXC_data$name == "fuel_level"),]
+headlamp_status = OpenXC_data[which(OpenXC_data$name == "headlamp_status"),]
+high_beam_status = OpenXC_data[which(OpenXC_data$name == "high_beam_status"),]
+ignition_status = OpenXC_data[which(OpenXC_data$name == "ignition_status"),]
+coordinates = OpenXC_data[which(OpenXC_data$name == "latitude" | OpenXC_data$name == "longitude"),]
+odometer = OpenXC_data[which(OpenXC_data$name == "odometer"),]
+parking_brake_status = OpenXC_data[which(OpenXC_data$name == "parking_brake_status"),]
+steering_wheel_angle = OpenXC_data[which(OpenXC_data$name == "steering_wheel_angle"),]
+torque_at_transmission = OpenXC_data[which(OpenXC_data$name == "torque_at_transmission"),]
+transmission_gear_position = OpenXC_data[which(OpenXC_data$name == "transmission_gear_position"),]
+vehicle_speed = OpenXC_data[which(OpenXC_data$name == "vehicle_speed"),]
+windshield_wiper_status = OpenXC_data[which(OpenXC_data$name == "windshield_wiper_status"),]
+
+################################################################################################
+################################################################################################
+################################################################################################
+# CONNECTING RESULTS TO MySQL database
+
+# Write Tables (if doing this the first time, change overwrite to T and change append to F)
+dbWriteTable(con, name="accelerator_pedal_position", 
+             value=accelerator_pedal_position,
+             field.types=list(trace="varchar(21)", value="double", name="varchar(40)", timestamp="int(10)"),
+             row.names=F, overwrite=F, append=T)
+
+dbWriteTable(con, name="brake_pedal_status", 
+             value=brake_pedal_status, 
+             field.types=list(trace="varchar(21)", value="varchar(4)",  name="varchar(40)", timestamp="int(10)"),
+             row.names=F, overwrite=F, append=T)
+
+dbWriteTable(con, name="brake_pedal_status", 
+             value=brake_pedal_status, 
+             field.types=list(trace="varchar(21)", value="varchar(4)",  name="varchar(40)", timestamp="int(10)"),
+             row.names=F, overwrite=F, append=T)
+
+dbWriteTable(con, name="engine_speed", 
+             value=engine_speed, 
+             field.types=list(trace="varchar(21)", value="double",  name="varchar(40)", timestamp="int(10)"),
+             row.names=F, overwrite=F, append=T)
+
+dbWriteTable(con, name="fuel_consumed_since_restart", 
+             value=fuel_consumed_since_restart, 
+             field.types=list(trace="varchar(21)", value="double",  name="varchar(40)", timestamp="int(10)"),
+             row.names=F, overwrite=F, append=T)
+
+dbWriteTable(con, name="fuel_level", 
+             value=fuel_level, 
+             field.types=list(trace="varchar(21)", value="double",  name="varchar(40)", timestamp="int(10)"),
+             row.names=F, overwrite=F, append=T)
+
+dbWriteTable(con, name="headlamp_status", 
+             value=headlamp_status, 
+             field.types=list(trace="varchar(21)", value="varchar(4)",  name="varchar(40)", timestamp="int(10)"),
+             row.names=F, overwrite=F, append=T)
+
+dbWriteTable(con, name="high_beam_status", 
+             value=high_beam_status, 
+             field.types=list(trace="varchar(21)", value="varchar(4)",  name="varchar(40)", timestamp="int(10)"),
+             row.names=F, overwrite=F, append=T)
+
+dbWriteTable(con, name="ignition_status", 
+             value=ignition_status, 
+             field.types=list(trace="varchar(21)", value="varchar(4)",  name="varchar(40)", timestamp="int(10)"),
+             row.names=F, overwrite=F, append=T)
+
+dbWriteTable(con, name="coordinates", 
+             value=coordinates, 
+             field.types=list(trace="varchar(21)", value="double",  name="varchar(40)", timestamp="int(10)"),
+             row.names=F, overwrite=F, append=T)
+
+dbWriteTable(con, name="odometer", 
+             value=odometer, 
+             field.types=list(trace="varchar(21)", value="double",  name="varchar(40)", timestamp="int(10)"),
+             row.names=F, overwrite=F, append=T)
+
+dbWriteTable(con, name="parking_brake_status", 
+             value=parking_brake_status, 
+             field.types=list(trace="varchar(21)", value="varchar(4)",  name="varchar(40)", timestamp="int(10)"),
+             row.names=F, overwrite=F, append=T)
+
+dbWriteTable(con, name="steering_wheel_angle", 
+             value=steering_wheel_angle, 
+             field.types=list(trace="varchar(21)", value="double",  name="varchar(40)", timestamp="int(10)"),
+             row.names=F, overwrite=F, append=T)
+
+dbWriteTable(con, name="torque_at_transmission", 
+             value=torque_at_transmission, 
+             field.types=list(trace="varchar(21)", value="double",  name="varchar(40)", timestamp="int(10)"),
+             row.names=F, overwrite=F, append=T)
+
+dbWriteTable(con, name="transmission_gear_position", 
+             value=transmission_gear_position, 
+             field.types=list(trace="varchar(21)", value="varchar(7)",  name="varchar(40)", timestamp="int(10)"),
+             row.names=F, overwrite=F, append=T)
+
+dbWriteTable(con, name="vehicle_speed", 
+             value=vehicle_speed, 
+             field.types=list(trace="varchar(21)", value="double",  name="varchar(40)", timestamp="int(10)"),
+             row.names=F, overwrite=F, append=T)
+
+dbWriteTable(con, name="windshield_wiper_status", 
+             value=windshield_wiper_status, 
+             field.types=list(trace="varchar(21)", value="varchar(4)",  name="varchar(40)", timestamp="int(10)"),
+             row.names=F, overwrite=F, append=T)
+
+# A couple of commands to confirm that the created tables exist: 
+#dbGetQuery(con, "select count(*) from brake_pedal_status;")
+#dbListTables(con)
+
+# Disconnect from Database
+dbDisconnect(con)
+dbUnloadDriver(drv)
