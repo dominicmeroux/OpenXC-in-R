0126b2e2eed0e6c331204d8d0673a36b39b09421
dominicmeroux <dmeroux@gmail.com>
2017-02-12 19:13:02 -0800

    Updates to SparkR code

--- SparkR.R
+++ SparkR.R
@@ -1,14 +1,25 @@
 # Databricks notebook source
+# MAGIC %md # Analysis of MY 2013 Ford Fiesta Drive Traces
+# MAGIC 
+# MAGIC Using Spark 2.1, Scala 2.1.1 Cluster on Databricks
+# MAGIC 
+# MAGIC Code syntax may need to be modified slightly for use with different versions of Spark 
+# MAGIC 
+# MAGIC ### TABLE OF CONTENTS
+# MAGIC 1) Door Status
+# MAGIC 
+# MAGIC 2) Harsh Acceleration
+# MAGIC 
+# MAGIC 3) Fuel Level
+# MAGIC 
+# MAGIC 4) Spatial Data
+
+# COMMAND ----------
+
 # Useful documentation: https://docs.databricks.com/spark/latest/sparkr/functions/unionAll.html
 
-# TABLE OF CONTENTS
-########################################################################################################################
-# Harsh Acceleration
-
-
-# Using Spark 2.0, Scala 2.1.0
 FilePath_3FADP4BJ5DM119777_1 <- '/FileStore/tables/ctihs8f51474399879780' # Data from 2015 - 2016, collected via Reference VI with Android phone by BlueTooth
-FilePath_3FADP4BJ5DM119777_2 <- '/FileStore/tables/kmmptxmn1485673267130' # Data from January 2017, collected via C5 BT with SD card
+FilePath_3FADP4BJ5DM119777_2 <- '/FileStore/tables/w1ddnp4s1486625886860' # Data from January 2017, collected via C5 BT with SD card
 
 df_3FADP4BJ5DM119777_1 <- read.df(FilePath_3FADP4BJ5DM119777_1, "json")
 df_3FADP4BJ5DM119777_2 <- read.df(FilePath_3FADP4BJ5DM119777_2, "json")
@@ -23,11 +34,11 @@ df_3FADP4BJ5DM119777_2 <- read.df(FilePath_3FADP4BJ5DM119777_2, "json")
 
 # (POOR PERFORMANCE) approach:
 # Create vector of VIN values, each the size of its corresponding Spark dataframe
-VIN_1 = rep("3FADP4BJ5DM119777", times=count(df_3FADP4BJ5DM119777_1))
-VIN_2 = rep("3FADP4BJ5DM119777", times=count(df_3FADP4BJ5DM119777_2))
+#VIN_1 = rep("3FADP4BJ5DM119777", times=count(df_3FADP4BJ5DM119777_1))
+#VIN_2 = rep("3FADP4BJ5DM119777", times=count(df_3FADP4BJ5DM119777_2))
 
 # Create Spark dataframe from concatenated (ordered) vectors
-VIN = as.DataFrame(data.frame(c(VIN_1, VIN_2)))
+#VIN = as.DataFrame(data.frame(c(VIN_1, VIN_2)))
 #DF2 <- merge(DF, VIN, all.x=FALSE, all.y=TRUE)
 # Cartesian joins could be prohibitively expensive and are disabled by default. To explicitly enable them, please set spark.sql.crossJoin.enabled = true;
 
@@ -40,9 +51,12 @@ df_3FADP4BJ5DM119777_2 <- select(df_3FADP4BJ5DM119777_2, df_3FADP4BJ5DM119777_2$
 
 # concatenate all dataframes into one large dataframe
 DF <- unionAll(df_3FADP4BJ5DM119777_1, df_3FADP4BJ5DM119777_2)
-#count(df_3FADP4BJ5DM119777_1) # 7245978
-#count(df_3FADP4BJ5DM119777_2) # 456 ########## TODO: fix issue with SD card files that causes only the first line of each to be read in
-#count(DF) # 7246434
+
+# COMMAND ----------
+
+#count(df_3FADP4BJ5DM119777_1) #  7,245,978
+#count(df_3FADP4BJ5DM119777_2) #  7,210,768
+#count(DF)                     # 14,456,746
 
 # COMMAND ----------
 
@@ -74,23 +88,49 @@ door_status                 <- filter(DF, DF$name == 'door_status')
 
 # COMMAND ----------
 
-head(door_status, 30)
+# Door information doesn't tell us much, except that in this case the car is mostly a single-occupant vehicle, and likely there will need to be debugging of this signal as it captured door values on only a subset of trips
+#head(filter(door_status, door_status$event == TRUE), 3)
+display(filter(door_status, door_status$event == TRUE))
 
 # COMMAND ----------
 
-# Proportion of acceleration events where acceleration events were "harsh" - accelerator pedal position more than 50%
-count(filter(accelerator_pedal_position, accelerator_pedal_position$value > 50)) / count(filter(accelerator_pedal_position, accelerator_pedal_position$value > 0))
+# Proportion of acceleration events where acceleration events were "harsh" - accelerator pedal position (APP) more than 50%
+count(filter(accelerator_pedal_position, accelerator_pedal_position$value > 50)) / count(filter(accelerator_pedal_position, accelerator_pedal_position$value > 0)) # 0.0009684567, or nearly 1/10th of a percent
 
 # COMMAND ----------
 
-display(filter(accelerator_pedal_position, accelerator_pedal_position$value > 50))
+count(filter(accelerator_pedal_position, accelerator_pedal_position$value > 50)) # 743 measurements (of 1464365 total) where APP was above 50%
 
 # COMMAND ----------
 
-# trying to show date of timestamp https://spark.apache.org/docs/1.5.1/api/R/unix_timestamp.html
-#A = withColumn(accelerator_pedal_position, "Date", to_date(as.integer(accelerator_pedal_position$timestamp))) ### ERRORS OUT
-#head(A)
+count(filter(accelerator_pedal_position, accelerator_pedal_position$value > 70)) # Just 73 measurements (of 1464365 total) where APP was above 70%
 
 # COMMAND ----------
 
+display(filter(accelerator_pedal_position, accelerator_pedal_position$value > 70))
 
+# COMMAND ----------
+
+collect(select(fuel_level, min(fuel_level$value))) # Lowest fuel level recorded was 12.5%
+
+# COMMAND ----------
+
+display(fuel_level)
+
+# COMMAND ----------
+
+registerTempTable(latitude, "latitudeDF")
+registerTempTable(longitude, "longitudeDF")
+CoordDF <- sql(sqlContext, "SELECT a.timestamp, a.value as Latitude, b.value as Longitude FROM latitudeDF as a, longitudeDF as b where a.timestamp = b.timestamp")
+
+# COMMAND ----------
+
+head(CoordDF, 3)
+
+# COMMAND ----------
+
+# TODO: Send spatial data and desired associated data to an external source for more in-depth analysis
+
+# COMMAND ----------
+
+# Machine Learning with SparkR: https://docs.databricks.com/spark/latest/sparkr/overview.html#machine-learning
